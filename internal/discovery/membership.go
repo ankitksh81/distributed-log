fc5ecc9eaa3ff4c1a0f3da827d899a9e24997eec
ankitksh81 <ankitksh81@gmail.com>
2022-01-05 22:07:46 +0530

    added functionality for service discovery and added tests

---
+++ internal/discovery/membership.go
@@ -0,0 +1,149 @@
+package discovery
+
+import (
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+type Membership struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	events  chan serf.Event
+	logger  *zap.Logger
+}
+
+// New(handler, config) creates a Membership with the required
+// configuration and even handler
+func New(handler Handler, config Config) (*Membership, error) {
+	c := &Membership{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+
+	if err := c.setupSerf(); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+// setupSerf() creates and configures a Serf instance and
+// starts the eventsHandler() goroutine to handler Serf's
+// events.
+func (m *Membership) setupSerf() (err error) {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
+	if err != nil {
+		return err
+	}
+
+	config := serf.DefaultConfig()
+	config.Init()
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindPort = addr.Port
+	m.events = make(chan serf.Event)
+	config.EventCh = m.events
+	config.Tags = m.Tags
+	config.NodeName = m.Config.NodeName
+	m.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+
+	go m.eventHandler()
+	if m.StartJoinAddrs != nil {
+		_, err = m.serf.Join(m.StartJoinAddrs, true)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+// eventHandler() runs in a loop reading events sent by Serf
+// into the events channel, handling each incoming event
+// according to event's type.
+func (m *Membership) eventHandler() {
+	for e := range m.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue
+				}
+				m.handleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					return
+				}
+				m.handleLeave(member)
+			}
+		}
+	}
+}
+
+// handleJoin(member) joins a new node to the cluster.
+// Serf sends an event to all nodes, including the node
+// that joined.
+func (m *Membership) handleJoin(member serf.Member) {
+	if err := m.handler.Join(
+		member.Name,
+		member.Tags["rpc_addr"],
+	); err != nil {
+		m.logError(err, "failed to join", member)
+	}
+}
+
+// handleLeave(member) leaves a new node to the cluster.
+// Serf sends an event to all nodes, including the node
+// that left.
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(
+		member.Name,
+	); err != nil {
+		m.logError(err, "failed to leave", member)
+	}
+}
+
+// isLocal(member) returns whether the given Serf member
+// is the local member by checking the members' names.
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+// Members() returns a point-in-time snapshot of the cluster's Serf
+// members.
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+// Leave() tells this member to leave the Serf cluster.
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
+
+// logError() logs the given error and message.
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	m.logger.Error(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rpc_addr", member.Tags["rpc_addr"]),
+	)
+}
---
+++ internal/discovery/membership_test.go
@@ -0,0 +1,97 @@
+package discovery_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/ankitksh81/distributed-log/internal/discovery"
+	"github.com/hashicorp/serf/serf"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+)
+
+// TestMembership() sets up a cluster with multiple
+// servers and checks that the Membership returns all
+// the servers that joined the membership and updates
+// after a server leaves the cluster.
+func TestMembership(t *testing.T) {
+	m, handler := setupMember(t, nil)
+	m, _ = setupMember(t, m)
+	m, _ = setupMember(t, m)
+
+	require.Eventually(t, func() bool {
+		return 2 == len(handler.joins) &&
+			3 == len(m[0].Members()) &&
+			0 == len(handler.leaves)
+	}, 3*time.Second, 250*time.Millisecond)
+	require.NoError(t, m[2].Leave())
+
+	require.Eventually(t, func() bool {
+		return 2 == len(handler.joins) &&
+			3 == len(m[0].Members()) &&
+			serf.StatusLeft == m[0].Members()[2].Status &&
+			1 == len(handler.leaves)
+	}, 3*time.Second, 250*time.Millisecond)
+	require.Equal(t, fmt.Sprintf("%d", 2), <-handler.leaves)
+}
+
+// setupMember() sets up a new member under a free port and
+// with the member's length as the node name so the names are
+// unique. Member's length also tells us whether this member is
+// the cluster's initial member or we have  a cluster to join.
+func setupMember(t *testing.T, members []*Membership) (
+	[]*Membership, *handler,
+) {
+	id := len(members)
+	ports := dynaport.Get(1)
+	addr := fmt.Sprintf("%s:%d", "127.0.0.1", ports[0])
+	tags := map[string]string{
+		"rpc_addr": addr,
+	}
+	c := Config{
+		NodeName: fmt.Sprintf("%d", id),
+		BindAddr: addr,
+		Tags:     tags,
+	}
+
+	h := &handler{}
+	if len(members) == 0 {
+		h.joins = make(chan map[string]string, 3)
+		h.leaves = make(chan string, 3)
+	} else {
+		c.StartJoinAddrs = []string{
+			members[0].BindAddr,
+		}
+	}
+
+	m, err := New(h, c)
+	require.NoError(t, err)
+	members = append(members, m)
+	return members, h
+}
+
+// The handler mock tracks how many times our Membership
+// calls the handler's Join() & Leaves() methods, and with
+// what IDs and addresses.
+type handler struct {
+	joins  chan map[string]string
+	leaves chan string
+}
+
+func (h *handler) Join(id, addr string) error {
+	if h.joins != nil {
+		h.joins <- map[string]string{
+			"id":   id,
+			"addr": addr,
+		}
+	}
+	return nil
+}
+
+func (h *handler) Leave(id string) error {
+	if h.leaves != nil {
+		h.leaves <- id
+	}
+	return nil
+}
