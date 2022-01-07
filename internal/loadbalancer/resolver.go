5033617786ac26b2c27decf03837c242d0004e52
ankitksh81 <ankitksh81@gmail.com>
2022-01-07 18:01:04 +0530

    Add: resolver handler to resolve the servers

---
+++ internal/loadbalancer/resolver.go
@@ -0,0 +1,112 @@
+package loadbalancer
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	api "github.com/ankitksh81/distributed-log/api/v1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/attributes"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/serviceconfig"
+)
+
+type Resolver struct {
+	mu            sync.Mutex
+	clientConn    resolver.ClientConn
+	resolverConn  *grpc.ClientConn
+	serviceConfig *serviceconfig.ParseResult
+	logger        *zap.Logger
+}
+
+var _ resolver.Builder = (*Resolver)(nil)
+
+// Build sets up a client connection to our server so the resolver can call
+// the GetServers() API.
+func (r *Resolver) Build(target resolver.Target,
+	cc resolver.ClientConn, opts resolver.BuildOptions,
+) (resolver.Resolver, error) {
+	r.logger = zap.L().Named("resolver")
+	r.clientConn = cc
+	var dialOpts []grpc.DialOption
+	if opts.DialCreds != nil {
+		dialOpts = append(
+			dialOpts,
+			grpc.WithTransportCredentials(opts.DialCreds),
+		)
+	}
+	// For our resolver, we'll format the target address like this:
+	// dlog://service-address
+	r.serviceConfig = r.clientConn.ParseServiceConfig(
+		fmt.Sprintf(`{"loadBalancingConfig":[{"%s":{}}]}`, Name),
+	)
+
+	var err error
+	r.resolverConn, err = grpc.Dial(target.Endpoint, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	r.ResolveNow(resolver.ResolveNowOptions{})
+	return r, nil
+}
+
+const Name = "dlog"
+
+// Scheme returns the resolver's scheme identifier.
+func (r *Resolver) Scheme() string {
+	return Name
+}
+
+// Init register the resolver with gRPC
+func init() {
+	resolver.Register(&Resolver{})
+}
+
+var _ resolver.Resolver = (*Resolver)(nil)
+
+// gRPC calls ResolveNow to resolve the target, discover the servers,
+// and update the client connection with servers.
+func (r *Resolver) ResolveNow(resolver.ResolveNowOptions) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	client := api.NewLogClient(r.resolverConn)
+	// get cluster and then set on cc attributes
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServersRequest{})
+	if err != nil {
+		r.logger.Error(
+			"failed to resolve server",
+			zap.Error(err),
+		)
+		return
+	}
+
+	var addrs []resolver.Address
+	for _, server := range res.Servers {
+		addrs = append(addrs, resolver.Address{
+			Addr: server.RpcAddr,
+			Attributes: attributes.New(
+				"is_leader",
+				server.IsLeader,
+			),
+		})
+	}
+
+	r.clientConn.UpdateState(resolver.State{
+		Addresses:     addrs,
+		ServiceConfig: r.serviceConfig,
+	})
+}
+
+// Close closes the resolver.
+func (r *Resolver) Close() {
+	if err := r.resolverConn.Close(); err != nil {
+		r.logger.Error(
+			"failed to close conn",
+			zap.Error(err),
+		)
+	}
+}
