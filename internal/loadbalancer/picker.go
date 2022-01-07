56096d138079448b29fce1f762167201c89d6a7a
ankitksh81 <ankitksh81@gmail.com>
2022-01-07 18:29:11 +0530

    Add: picker handler to handle RPC balancing logic

---
+++ internal/loadbalancer/picker.go
@@ -0,0 +1,74 @@
+package loadbalancer
+
+import (
+	"strings"
+	"sync"
+	"sync/atomic"
+
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+)
+
+var _ base.PickerBuilder = (*Picker)(nil)
+
+type Picker struct {
+	mu        sync.Mutex
+	leader    balancer.SubConn
+	followers []balancer.SubConn
+	current   uint64
+}
+
+// Build set up the picker, gRPC connected to the addresses that our
+// resolver discovered.
+func (p *Picker) Build(buildInfo base.PickerBuildInfo) balancer.Picker {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	var followers []balancer.SubConn
+	for sc, scInfo := range buildInfo.ReadySCs {
+		isLeader := scInfo.Address.Attributes.Value("is_leader").(bool)
+
+		if isLeader {
+			p.leader = sc
+			continue
+		}
+		followers = append(followers, sc)
+	}
+	p.followers = followers
+	return p
+}
+
+var _ balancer.Picker = (*Picker)(nil)
+
+// Pick returns a balancer.PickResult with the subconnection to handle the
+// call.
+func (p *Picker) Pick(info balancer.PickInfo) (
+	balancer.PickResult, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	var result balancer.PickResult
+	if strings.Contains(info.FullMethodName, "Produce") ||
+		len(p.followers) == 0 {
+		result.SubConn = p.leader
+	} else if strings.Contains(info.FullMethodName, "Consume") {
+		result.SubConn = p.nextFollower()
+	}
+	if result.SubConn == nil {
+		return result, balancer.ErrNoSubConnAvailable
+	}
+	return result, nil
+}
+
+func (p *Picker) nextFollower() balancer.SubConn {
+	cur := atomic.AddUint64(&p.current, uint64(1))
+	len := uint64(len(p.followers))
+	idx := int(cur % len)
+	return p.followers[idx]
+}
+
+func init() {
+	balancer.Register(
+		base.NewBalancerBuilder(Name, &Picker{}, base.Config{}),
+	)
+}
